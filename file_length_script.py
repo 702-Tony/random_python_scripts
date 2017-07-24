a4502814871a08943c4fdaa9dc82322a0cfd0acc
702-Tony <aadams702@hotmail.com>
2017-07-24 15:37:42 -0700

    implemented output types
    added option to change output to csv file and made serverpath reflect drive length if not on server drive

--- file_length_script.py
+++ file_length_script.py
@@ -1,17 +1,28 @@
 import os
-
-#Runs on Windows only
+import sys
+#tested on Windows only
 
 print 'Welcome! this program will find files with pathnames that are too long'.center(80, '+')
+if len(sys.argv) > 1:
+    if sys.argv[1] == '--csv':
+        filename = raw_input('Name output file(file output in .csv format): ')
+        if filename[-4:] != '.csv': filename += '.csv'
+        csv = True
+    elif sys.argv[1] == '--txt' :
+        filename = raw_input('Name output file(file output in .txt format): ')
+        if filename[-4:] != '.txt': filename += '.txt'
+        csv = False
+    else :
+        filename = raw_input('Name output file(file output in .txt format): ')
+        if filename[-4:] != '.txt': filename += '.txt'
+        csv = False
 
-filename = raw_input('Name output file: ')
-
-if filename[-4:] != '.txt': filename += '.txt'
 
 outputfile = open(filename,'w')# opens file matching name, otherwise creates file if not found
 
 
 dir = str(raw_input('Which directory? '))
+#below, serverpath adds length to file path to represent server path since running remotely
 if dir[0] == 'J':
     serverpath = 11
 elif dir[0] == 'K':
@@ -25,22 +36,27 @@ count = 0
 
 print 'working...'
 
+if csv == True: outputfile.write('File Path Length,Filepath 1, Filepath 2\n')
+
 for root, dirs, files in os.walk('.'):
     
     for i in files:
-        
+        if serverpath == 0: serverpath = len(dir)#changes serverpath to length reflected on current drive if not server drive J or K
         x = (root +'\\' + i )[2:] #removes ./ from filename and assigns full filepath to directory
         
         if (len(x)+serverpath) > 255:
             #print 'Files written: ' + str(count +1)
             #print (dir + '\\' + x + '\n')
             #print len(x)
-            outputfile.write(dir + '\\' + x + '\n')
+            if csv == True:
+                outputfile.write(str(len(x)+serverpath)+','+ dir + ',' + x + '\n')
+            else: outputfile.write(dir + '//' + x + '\n')
+            
             count +=1
     
     
 
-print 'Done: total lines written: ' + str(count)
+print 'Done: total files found: ' + str(count)
 
 outputfile.close()
 
